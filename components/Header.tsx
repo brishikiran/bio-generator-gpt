c9d0f46432b445a75c8ba52b98972c0e02223323
rishi <rishi.123kiran@gmail.com>
2024-03-10 19:57:20 -0400

    gpt bio

---
+++ components/Header.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import Github from './GitHub';
+
+export default function Header() {
+  return (
+<header className="flex justify-center items-center w-full mt-5 border-b-2 pb-7 sm:px-4 px-2">
+  <Link href="/" className="flex space-x-3 items-center">
+    <img
+      alt="header text"
+      src="/write.svg"
+      className="sm:w-9 sm:h-9 w-8 h-8"
+    />
+    <h1 className="sm:text-3xl text-2xl font-bold ml-2 tracking-tight">
+      Bio generator
+    </h1>
+  </Link>
+</header>
+
+  );
+}
